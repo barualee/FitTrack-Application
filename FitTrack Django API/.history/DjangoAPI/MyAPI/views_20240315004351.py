1a3490d8f0ecf5416c038f2fd20ca4a8f9332e52
Nishant Kumar Barua <barua.nishant97@gmail.com>
2024-11-09 00:04:04 -0500

    Delete FitTrack Django API/.history/DjangoAPI directory
    deleting bloat

--- FitTrack Django API/.history/DjangoAPI/MyAPI/views_20240315004351.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def prediction(request):
-     response=JsonResponse({'predicted':'predicted'})
-     return set_cors_headers(response)
